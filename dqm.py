8d94a69840f0703567ea54f51f187f8e32f8edd9
Leszek Grzanka <grzanka@agh.edu.pl>
2023-09-10 23:35:52 +0200

    Create dqm.py

---
+++ dqm.py
@@ -0,0 +1,66 @@
+import FWCore.ParameterSet.Config as cms
+import string
+
+from Configuration.Eras.Era_Run3_cff import Run3
+
+process = cms.Process('RECODQM', Run3)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+#process.verbosity = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+# minimum of logs
+process.MessageLogger = cms.Service("MessageLogger",
+    statistics = cms.untracked.vstring(),
+    destinations = cms.untracked.vstring('cerr'),
+    cerr = cms.untracked.PSet(
+        threshold = cms.untracked.string('WARNING')
+    )
+)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# load DQM framework
+process.load("DQM.Integration.config.environment_cfi")
+process.dqmEnv.subSystemFolder = "CTPPS"
+process.dqmEnv.eventInfoFolder = "EventInfo"
+process.dqmSaver.path = ""
+process.dqmSaver.tag = "CTPPS"
+# raw data source
+process.source = cms.Source("PoolSource",    
+    fileNames = cms.untracked.vstring()
+)
+
+#Latest PPS GT candidate
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '130X_dataRun3_HLT_v2', '')
+
+#Raw-to-digi
+process.load('EventFilter.CTPPSRawToDigi.ctppsRawToDigi_cff')
+
+# local RP reconstruction chain with standard settings
+process.load("RecoPPS.Configuration.recoCTPPS_cff")
+
+# CTPPS DQM modules
+process.load("DQM.CTPPS.ctppsDQM_cff")
+
+process.path = cms.Path(
+  process.ctppsRawToDigi *
+  process.recoCTPPS *
+  process.ctppsDQMCalibrationSource *
+  process.ctppsDQMCalibrationHarvest
+)
+
+process.end_path = cms.EndPath(
+    process.dqmEnv +
+    process.dqmSaver
+)
+
+process.schedule = cms.Schedule(
+    process.path,
+    process.end_path
+)
+
